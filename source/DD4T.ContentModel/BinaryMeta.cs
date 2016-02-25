c9485f1c9eed3b903a2e8d5c17b77d98e0136b31
Quirijn Slings <quirijn.slings@trivident.com>
2016-02-25 17:52:16 +0100

    Added IBinaryMeta & BinaryMeta (to be used for invalidation of binaries from the cache)

---
+++ source/DD4T.ContentModel/BinaryMeta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DD4T.ContentModel
+{
+    [Serializable]
+    public class BinaryMeta : IBinaryMeta
+    {
+        public string VariantId { get; set; }
+        public string Id { get; set; }
+        public DateTime LastPublishedDate { get; set; }
+        public bool HasLastPublishedDate { get; set; }
+    }
+}
