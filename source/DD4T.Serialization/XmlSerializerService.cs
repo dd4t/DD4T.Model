8c8a3b77c43d134d02d8c1b958f9d79ed8aa33bd
sshibani <siawash.shibani@trivident.com>
2016-09-07 12:57:05 +0200

    extend Keyword model with additional properties #23

--- source/DD4T.Serialization/XmlSerializerService.cs
+++ source/DD4T.Serialization/XmlSerializerService.cs
@@ -1,23 +1,24 @@
-﻿using System;
+﻿using DD4T.ContentModel;
+using DD4T.ContentModel.Contracts.Serializing;
+using Microsoft.Xml.Serialization.GeneratedAssembly;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using DD4T.ContentModel;
-using System.IO;
+using System.Text.RegularExpressions;
 using System.Xml;
 using System.Xml.Serialization;
-using Microsoft.Xml.Serialization.GeneratedAssembly;
-using System.Text.RegularExpressions;
-using DD4T.ContentModel.Contracts.Serializing;
 
 namespace DD4T.Serialization
 {
     public class XmlSerializerService : BaseSerializerService
     {
         private static Dictionary<Type, XmlSerializer> _xmlSerializers = new Dictionary<Type, XmlSerializer>();
-        private XmlSerializer GetXmlSerializer<T>() where T: XmlSerializer
+
+        private XmlSerializer GetXmlSerializer<T>() where T : XmlSerializer
         {
-            if (! _xmlSerializers.ContainsKey(typeof(T)))
+            if (!_xmlSerializers.ContainsKey(typeof(T)))
             {
                 XmlSerializer serializer = (T)Activator.CreateInstance(typeof(T));
                 _xmlSerializers.Add(typeof(T), serializer);
@@ -36,7 +37,7 @@ namespace DD4T.Serialization
 
             //Add an empty namespace and empty value
             ns.Add("", "");
-                       
+
             serializer.Serialize(writer, o, ns);
             outputValue = Encoding.UTF8.GetString(ms.ToArray());
 
@@ -47,8 +48,7 @@ namespace DD4T.Serialization
             return outputValue;
         }
 
-
-        public override string Serialize<T>(T input) 
+        public override string Serialize<T>(T input)
         {
             string result;
             if (input is Page || input is IPage)
@@ -70,14 +70,13 @@ namespace DD4T.Serialization
             return ((SerializationProperties)SerializationProperties).CompressionEnabled ? Compressor.Compress(result) : result;
         }
 
-        public override T Deserialize<T>(string input) 
+        public override T Deserialize<T>(string input)
         {
             // NOTE: important exception situation!!
-            // if the requested type is IComponentPresentation, there is a possiblity that the data 
-            // provided to us actually contains a Component instead. In that case we need to add a 
+            // if the requested type is IComponentPresentation, there is a possiblity that the data
+            // provided to us actually contains a Component instead. In that case we need to add a
             // dummy CT / CP around the Component and return that!
 
-            
             if (((SerializationProperties)SerializationProperties).CompressionEnabled)
             {
                 input = Compressor.Decompress(input);
@@ -102,7 +101,7 @@ namespace DD4T.Serialization
                 {
                     // handle the exception situation where we are asked to deserialize into a CP but the data is actually a Component
                     serializer = GetXmlSerializer<ComponentSerializer>();
-                    Component component = (Component) serializer.Deserialize(tr);
+                    Component component = (Component)serializer.Deserialize(tr);
                     IComponentPresentation componentPresentation = new ComponentPresentation()
                     {
                         Component = component,
@@ -115,12 +114,12 @@ namespace DD4T.Serialization
             return (T)serializer.Deserialize(tr);
         }
 
-
         public override bool IsAvailable()
         {
             return Type.GetType("Microsoft.Xml.Serialization.GeneratedAssembly.ComponentSerializer") != null;
         }
     }
+
     public class XmlTextWriterFormattedNoDeclaration : XmlTextWriter
     {
         public XmlTextWriterFormattedNoDeclaration(System.IO.TextWriter w)
@@ -128,11 +127,15 @@ namespace DD4T.Serialization
         {
             Formatting = System.Xml.Formatting.Indented;
         }
+
         public XmlTextWriterFormattedNoDeclaration(System.IO.MemoryStream ms, Encoding enc)
             : base(ms, enc)
         {
             Formatting = System.Xml.Formatting.Indented;
         }
-        public override void WriteStartDocument() { } // suppress
+
+        public override void WriteStartDocument()
+        {
+        } // suppress
     }
 }
