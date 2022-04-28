fae19bf9d1f63753c30c40ecc3f2b03af2f4e6b5
Quirijn Slings <quirijn.slings@trivident.com>
2022-04-28 21:09:04 +0200

    added DD4T.JsonSerializerMaxDepth appSetting to override Newtonsoft default

---
+++ source/DD4T.Serialization/Configuration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace DD4T.Serialization
+{
+    public static class Configuration
+    {
+        public static int JsonSerializerMaxDepth => ToInt(ConfigurationManager.AppSettings["DD4T.JsonSerializerMaxDepth"]);
+
+        private static int ToInt(string v)
+        {
+            return v == null ? default(int) : Convert.ToInt32(v);
+        }
+    }
+}
