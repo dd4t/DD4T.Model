305fb7a8773fed288ad2c7c4cee611dfffabe0f0
Quirijn Slings <quirijn.slings@trivident.com>
2022-07-06 21:53:20 +0200

    default depth for NewtonSoft now set to 128

--- source/DD4T.Serialization/Configuration.cs
+++ source/DD4T.Serialization/Configuration.cs
@@ -8,11 +8,11 @@ namespace DD4T.Serialization
 {
     public static class Configuration
     {
-        public static int JsonSerializerMaxDepth => ToInt(ConfigurationManager.AppSettings["DD4T.JsonSerializerMaxDepth"]);
+        public static int JsonSerializerMaxDepth => ToInt(ConfigurationManager.AppSettings["DD4T.JsonSerializerMaxDepth"], 128);
 
-        private static int ToInt(string v)
+        private static int ToInt(string v, int defaultValue)
         {
-            return v == null ? default(int) : Convert.ToInt32(v);
+            return v == null ? defaultValue: Convert.ToInt32(v);
         }
     }
 }
